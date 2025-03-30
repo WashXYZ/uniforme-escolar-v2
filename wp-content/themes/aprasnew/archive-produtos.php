b550f58daffed9bfb95af28ca8b7abaa9c20a05d
Washington Meneses <washington@wsxapps.com.br>
2025-03-30 15:21:57 -0300

    git ignore

--- wp-content/themes/aprasnew/archive-produtos.php
+++ wp-content/themes/aprasnew/archive-produtos.php
@@ -22,8 +22,13 @@
 <?php $loop = new WP_Query( array( 'post_type' => 'produtos', 'posts_per_page' => -1,  'order' => 'ASC', 'category' => 'current' ) ); ?>
 <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
-<div class="col-xl-3 col-md-6 col-sm-6 col-lg-4 col-12 text-center mb-20">
-<?php the_post_thumbnail(array("class" => " p-25 case-thumbnail")); ?>
+<div class="col-xl-6 col-md-6 col-sm-6 col-lg-6 col-12 text-center mb-20">
+
+
+<?php if ( has_post_thumbnail()) {
+   $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large');
+   echo '<img src="' . $large_image_url[0] . '" title="' . the_title_attribute('echo=0') . '" class="produto-thumbnail img-fluid p-150" />';
+ } ?>
 </div>
 
 
@@ -33,3 +38,11 @@
 
 </div>
 <?php get_footer(); ?>
+
+<style> 
+.produto-thumbnail {
+    box-shadow: 0 20px 40px 10px rgba(0, 0, 0, .1);
+    padding: 100px 0;
+    border-radius: 0 40px 40px;
+}
+</style>
